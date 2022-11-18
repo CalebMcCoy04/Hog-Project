ef86e16b4035d8d69139f32815cf093734d49759
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-11-18 10:08:33 -0600

    add something

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
-function Home(){
 
+function Home(){
     
+
     return(
-    <div className="home">
+    <div className="gif">
+       
+
+        
+        <div className="home">
         <h1 ><strong>Welcome To The Hog Team</strong></h1>
+        </div>
+
         
     </div>
     )
